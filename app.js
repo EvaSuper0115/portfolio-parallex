f361c2b7e11f4dd6e50a7ef34fc55dd2a6508357
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-05-12 18:01:17 +0100

    parallex effect finished for introduction

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const githubBoxContainer = document.querySelector(".box-container");
+const hiTextContainer = document.querySelector(".hiText-container");
+const profilePicContainer = document.querySelector(".image-container");
+const myName = document.querySelector(".imOnTai");
+const jobTitleContainer = document.querySelectorAll(".job-title-container");
+window.addEventListener("scroll", () => {
+  let offsetY = window.scrollY;
+  hiTextContainer.style.transform = `translate(${-offsetY * 0.2}px,${
+    -offsetY * 0.1
+  }px)`;
+  profilePicContainer.style.transform = `translate(${offsetY * 0.1}px,${
+    offsetY * 0.2
+  }px)`;
+  myName.style.transform = `translateX(${offsetY * 0.2}px)`;
+  jobTitleContainer[0].style.backgroundPositionY = `${offsetY * 0.5}px`;
+  jobTitleContainer[1].style.backgroundPositionY = `${-offsetY * 0.5}px`;
+});
