8a0e2072a0d259bb94da881c0917d7b7cb9e53da
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-05-12 18:16:11 +0100

    adding parallex effect to project section

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hiTextContainer = document.querySelector(".hiText-container");
 const profilePicContainer = document.querySelector(".image-container");
 const myName = document.querySelector(".imOnTai");
 const jobTitleContainer = document.querySelectorAll(".job-title-container");
+
 window.addEventListener("scroll", () => {
   let offsetY = window.scrollY;
   hiTextContainer.style.transform = `translate(${-offsetY * 0.2}px,${
@@ -12,5 +13,5 @@ window.addEventListener("scroll", () => {
   }px)`;
   myName.style.transform = `translateX(${offsetY * 0.2}px)`;
   jobTitleContainer[0].style.backgroundPositionY = `${offsetY * 0.5}px`;
-  jobTitleContainer[1].style.backgroundPositionY = `${-offsetY * 0.5}px`;
+  jobTitleContainer[1].style.backgroundPositionY = `${-offsetY * 0.4}px`;
 });
