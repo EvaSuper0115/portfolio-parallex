3810f2459e5b557b830a6e09a7b546edc8c9b770
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-05-13 17:34:15 +0100

    first project parallex effect

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const myName = document.querySelector(".imOnTai");
 const jobTitleContainer = document.querySelectorAll(".job-title-container");
 const jobTitle1 = document.querySelector(".front-end-developer-title");
 const jobTitle2 = document.querySelector(".web-designer-title");
+const projectTitle = document.querySelectorAll(".project-title");
+const projectDetail = document.querySelectorAll(".project-detail");
+const appShowcase = document.querySelectorAll(".app-showcase");
 window.addEventListener("scroll", () => {
   let offsetY = window.scrollY;
   hiTextContainer.style.transform = `translate(${-offsetY * 0.2}px,${
@@ -15,6 +18,13 @@ window.addEventListener("scroll", () => {
   myName.style.transform = `translateX(${offsetY * 0.2}px)`;
   jobTitleContainer[0].style.backgroundPositionY = `${offsetY * 0.5}px`;
   jobTitleContainer[1].style.backgroundPositionY = `${-offsetY * 0.5}px`;
-  jobTitle1.style.transform = `translateX(calc(200vh - ${offsetY * 0.9}px))`;
-  jobTitle2.style.transform = `translateX(calc(-200vh + ${offsetY * 0.8}px))`;
+  jobTitle1.style.transform = `translateX(calc(200vh - ${offsetY}px))`;
+  jobTitle2.style.transform = `translateX(calc(-300vh + ${offsetY}px))`;
+  projectTitle[0].style.transform = `translateY(calc(400vh - ${offsetY}px))`;
+  projectDetail[0].style.transform = `translateY(calc(400vh - ${
+    offsetY * 0.9
+  }px))`;
+  appShowcase[0].style.transform = `translateX(calc(500vh - ${
+    offsetY * 1.1
+  }px))`;
 });
