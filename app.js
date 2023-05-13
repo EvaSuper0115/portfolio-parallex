79de5d266314699ce28c9a1b4e7dd39c04ccfcc3
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-05-13 16:28:11 +0100

    job titles transformX

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const hiTextContainer = document.querySelector(".hiText-container");
 const profilePicContainer = document.querySelector(".image-container");
 const myName = document.querySelector(".imOnTai");
 const jobTitleContainer = document.querySelectorAll(".job-title-container");
-
+const jobTitle1 = document.querySelector(".front-end-developer-title");
+const jobTitle2 = document.querySelector(".web-designer-title");
 window.addEventListener("scroll", () => {
   let offsetY = window.scrollY;
   hiTextContainer.style.transform = `translate(${-offsetY * 0.2}px,${
@@ -14,4 +15,6 @@ window.addEventListener("scroll", () => {
   myName.style.transform = `translateX(${offsetY * 0.2}px)`;
   jobTitleContainer[0].style.backgroundPositionY = `${offsetY * 0.5}px`;
   jobTitleContainer[1].style.backgroundPositionY = `${-offsetY * 0.5}px`;
+  jobTitle1.style.transform = `translateX(${offsetY * 0.09}px)`;
+  jobTitle2.style.transform = `translateX(${-offsetY * 0.2}px)`;
 });
